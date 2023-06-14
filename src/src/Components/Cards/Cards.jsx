318f5d673cd0fef752b17514be4ab5d5a2bce9b8
sebastian-helguero <102705594+sebastian-helguero@users.noreply.github.com>
2023-06-14 11:29:25 -0300

    Add files via upload

---
+++ src/src/Components/Cards/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import "./Cards.css"
+
+const Cards = () => {
+return (
+    <div className="card">
+        <div className="first-content">
+            <span>Nombre</span>
+        </div>
+        <div className="second-content">
+            <span>Ingredientes</span>
+        </div>
+    </div>
+    )
+}
+
+export default Cards
---
+++ src/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+return (
+    <>
+        <nav>
+            <span> Main </span>
+            <div>
+                <span> Buscador </span>
+                <span> Configuracion </span>
+                <span> Cuenta </span>
+            </div>
+        </nav>
+    </>
+)
+}
+
+export default NavBar
